ff515450342774b43055cbb46ec5b81d79d42eb9
rydus1214 <43768288+rydus1214@users.noreply.github.com>
2019-05-16 09:04:48 +0900

    Revert "Revert "네이버 지도 테스트 중 by 교연""
    This reverts commit a34ba3a323d1756491d0aee34af102061c343b22.

--- capstone190510/capstone/app/src/main/java/com/example/capstone/MapActivity.java
+++ capstone190510/capstone/app/src/main/java/com/example/capstone/MapActivity.java
@@ -1,19 +1,35 @@
 package com.example.capstone;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        //
     }
 }
